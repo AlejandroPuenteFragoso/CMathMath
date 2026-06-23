52279d0ae8d773ce609df5359bafd233802ebe50
Alejandro Puente <alejandropufa@gmail.com>
2026-06-22 22:16:33 -0600

    Fix:carracteres invalidos soportados como UNKNOWN no como EOF

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -103,6 +103,10 @@ void Lexer::scanTokens()
         advance();
         std::string tokenValue(1, c);
         tokenType type = createToken(tokenValue);
+        if (type == tokenType::UNKNOWN) {
+            throw std::runtime_error(
+            std::string("Caracter inesperado: '") + c + "'");
+        }
         tokens.push_back(Token(type, tokenValue));
     }
     tokens.push_back(Token(tokenType::EOF_TOKEN, ""));
@@ -135,6 +139,7 @@ tokenType Lexer::createToken(const std::string& value)
             }
         }
         if (hasDigits) return tokenType::NUMBER;
+        else return tokenType::UNKNOWN;
     }
 
     return tokenType::EOF_TOKEN;
